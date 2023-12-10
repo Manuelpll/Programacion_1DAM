1ea5da8b0fa89d5993112fddd0281cc8aee6d989
Mparr <mparrallanson@gmail.com>
2023-12-10 18:15:52 +0100

    He quitado un cometario no necesario y he renombrado a un programa

--- src/Tema_3_Estructuras_de_control_y_Muchos_usos/Ejercicio06.java
+++ src/Tema_3_Estructuras_de_control_y_Muchos_usos/Ejercicio06.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tema_3_Estructuras_de_control_y_Muchos_usos;
 
 /**
