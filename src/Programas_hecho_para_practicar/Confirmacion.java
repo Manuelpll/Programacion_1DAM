78b17eb6a45908f31101b5df7e79b182d9278fe8
= <=>
2024-01-29 10:36:57 +0100

    Aprendiendo a poner ventanas si o no

---
+++ src/Programas_hecho_para_practicar/Confirmacion.java
@@ -0,0 +1,15 @@
+package Programas_hecho_para_practicar;
+
+import javax.swing.*;
+public class Confirmacion {
+    public static void main(String[] args) {
+
+        int respuesta = JOptionPane.showConfirmDialog(null, "¿David es feminista?", "Decision importante", JOptionPane.YES_NO_OPTION);
+
+        if (respuesta == JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Cagaste","F",3);
+        }else{
+            System.out.println("Elegiste bien");    JOptionPane.showMessageDialog(null,"Elegiste bien","GG",3);
+        }
+    }
+}
