009ed9e5b5209879ab4a409851e870e50aff1052
Manuel <=>
2024-02-06 13:27:21 +0100

    Casi finalizacion del ejercicio 3

--- src/Tema_6_Estructuras_de_Almacenamiento/ejercicio_3_Maquina_Expendedora/Maquina_Expendedora.java
+++ src/Tema_6_Estructuras_de_Almacenamiento/ejercicio_3_Maquina_Expendedora/Maquina_Expendedora.java
@@ -8,7 +8,7 @@ public class Maquina_Expendedora {
         int eleccion;
         boolean salir = false;
         Bebidas[] bebidas = new Bebidas[5];
-        bebidas[0] = new Bebidas("Cocacola", 1, 5);
+        bebidas[0] = new Bebidas("Cocacola", 1.00, 5);
         bebidas[1] = new Bebidas("Zumo de naranja", 0.75, 5);
         bebidas[2] = new Bebidas("Zumo de pina", 0.70, 5);
         bebidas[3] = new Bebidas("agua mineral con gas", 0.60, 5);
@@ -33,7 +33,7 @@ public class Maquina_Expendedora {
                     Pulsa 0 para salir
                                 """);
             eleccion = scanner.nextInt();
-
+               scanner.nextLine();
             switch (eleccion) {
                 case 1:
                     System.out.println("Los productos son los siguientes: ");
@@ -48,26 +48,83 @@ public class Maquina_Expendedora {
                     }
                     break;
                 case 2:
-                    System.out.println("Estos son los productos que quedan: ");
+                    System.out.println("Estos son los productos que hay ");
                     for (int i = 0; i < sandwichs.length; i++) {
-                        if (sandwichs[i].getCantidad()<0) {
                             System.out.println(sandwichs[i].getNombre() + ": "+sandwichs[i].getCantidad());
-                        }
                     }
                     for (int j = 0; j < dulces.length; j++) {
-                        if (dulces[j].getCantidad()<0) {
                             System.out.println(dulces[j].getNombre() + ": " + dulces[j].getCantidad());
-                        }
                     }
                     for (int h = 0; h < bebidas.length; h++) {
-                        if (bebidas[h].getCantidad() < 0) {
                             System.out.println(bebidas[h].getNombre() + ": " + bebidas[h].getCantidad());
-                        }
                     }
+
                     break;
                 case 3:
+                    System.out.println("Productos disponibles y sus cantidades actuales:");
+                    for (int i = 0; i < sandwichs.length; i++) {
+                        System.out.println(sandwichs[i].getNombre() + ": " + sandwichs[i].getPrecio() + " euros");
+                    }
+                    for (int j = 0; j < dulces.length; j++) {
+                        System.out.println( dulces[j].getNombre() + ": " + dulces[j].getPrecio() + " euros");
+                    }
+                    for (int h = 0; h < bebidas.length; h++) {
+                        System.out.println( bebidas[h].getNombre() + ": " + bebidas[h].getPrecio() + " euros");
+                    }
+                    scanner.nextLine();
+                    System.out.println("Elige producto que comprar:");
+                    String productoQueElige = scanner.nextLine();
+                    System.out.println("Inserte el dinero con el que va ha pagar");
+                    double cantidadDepositada=scanner.nextDouble();
+                    double cantidadFinal=0;
+                    for (int i=0;i< bebidas.length;i++) {
+                        if (productoQueElige.equalsIgnoreCase(String.valueOf(bebidas[i]))){
+                           cantidadFinal= cantidadDepositada-bebidas[i].getPrecio();
+                            bebidas[i].setCantidad(bebidas[i].getCantidad()-1);
+                        }//Fin primer if
+                    }
+                    for (int j=0;j <dulces.length;j++){
+                        if (productoQueElige.equalsIgnoreCase(String.valueOf(dulces[j]))){
+                            cantidadFinal= cantidadDepositada-dulces[j].getPrecio();
+                            dulces[j].setCantidad(dulces[j].getCantidad()-1);
+                        }//Fin segundo if
+                    }
+                    for (int h=0; h< sandwichs.length;h++){
+                        if (productoQueElige.equalsIgnoreCase(String.valueOf(sandwichs[h]))){
+                            cantidadFinal= cantidadDepositada-sandwichs[h].getPrecio();
+                            bebidas[h].setCantidad(bebidas[h].getCantidad()-1);
+                        }//Fin tercer if
+                    }
+                    System.out.println("La vuelta es: "+cantidadFinal);
                     break;
                 case 4:
+                    System.out.println("Productos disponibles y sus cantidades actuales:");
+                    for (int i = 0; i < sandwichs.length; i++) {
+                        System.out.println(sandwichs[i].getNombre() + ": " + sandwichs[i].getCantidad() + " disponibles");
+                    }
+                    for (int j = 0; j < dulces.length; j++) {
+                        System.out.println( dulces[j].getNombre() + ": " + dulces[j].getCantidad() + " disponibles");
+                    }
+                    for (int h = 0; h < bebidas.length; h++) {
+                        System.out.println( bebidas[h].getNombre() + ": " + bebidas[h].getCantidad() + " disponibles");
+                    }
+                    System.out.println("Elige producto reponer:");
+                    String productoQueRepone = scanner.nextLine();
+                    for (int i=0;i< bebidas.length;i++) {
+                        if (productoQueRepone.equalsIgnoreCase(String.valueOf(bebidas[i].getNombre()))){
+                            bebidas[i].setCantidad(5);
+                        }//Fin primer if
+                    }
+                    for (int j=0;j <dulces.length;j++){
+                        if (productoQueRepone.equalsIgnoreCase(String.valueOf(dulces[j].getNombre()))){
+                            dulces[j].setCantidad(5);
+                        }//Fin segundo if
+                    }
+                    for (int h=0; h< dulces.length;h++){
+                        if (productoQueRepone.equalsIgnoreCase(bebidas[h].getNombre())){
+                            bebidas[h].setCantidad(5);
+                        }//Fin tercer if
+                    }
                     break;
                 case 0:
                     System.out.println("Saliendo...");
